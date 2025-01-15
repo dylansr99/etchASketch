c273d1fcd9507f25fd1e12fc1fff28022a5b4947
dylansr99 <dylansr99@gmail.com>
2025-01-15 10:15:15 +0000

    Create function to add background-colour to divs

--- etchASketch.js
+++ etchASketch.js
@@ -1,12 +1,20 @@
 function generateNumberOfSquares(input) {
-    
-    for (let i = 0; i < input; i++) {
+
+    for (let i = 0; i < input * input; i++) {
       const square = document.createElement('div')
       square.classList.add("squares")
+      square.style.cssText = ''
       container.appendChild(square)
     }
    
-}
+};
 
-generateNumberOfSquares(16)
+generateNumberOfSquares(16);
+
+
+let squares = document.querySelectorAll('.squares')
+squares.forEach(square => square.addEventListener('mouseover', () => {
+     square.style.cssText = "background-color: black"
+}));
+
 
