5f082580c303833246b41af99f06bfd2e1dccc08
dylansr99 <dylansr99@gmail.com>
2025-01-19 15:13:07 +0000

    Add logic to size elements in JS, ammend color function

--- etchASketch.js
+++ etchASketch.js
@@ -1,20 +1,26 @@
 function generateNumberOfSquares(input) {
 
     for (let i = 0; i < input * input; i++) {
+      
+      let squareHeight = 600 / input
+      let squareWidth = 600 / input
+
       const square = document.createElement('div')
       square.classList.add("squares")
-      square.style.cssText = ''
+      square.setAttribute('style', `height: ${squareHeight}px; width: ${squareWidth}px` )
       container.appendChild(square)
     }
    
 };
 
-generateNumberOfSquares(16);
-
+generateNumberOfSquares(16)
 
 let squares = document.querySelectorAll('.squares')
 squares.forEach(square => square.addEventListener('mouseover', () => {
-     square.style.cssText = "background-color: black"
+     square.style.backgroundColor = "black"
 }));
+
+let newGridButton = document.querySelector('#generateNewGrid')
+
 
 
